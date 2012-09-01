9f248333b2a573732779dc936f2ae7e5723b3d66
Elina K. <elina.kaneva@gmail.com>
2012-09-01 23:26:21 +0300

    renaming some variables and images

--- tron.py
+++ tron.py
@@ -7,15 +7,15 @@ from random import uniform
 
 class Menu:
     def __init__(self):
-        self.main_menu = pygame.image.load("menu_1.png")
-        self.main_menu_light = pygame.image.load("glow.png")
-        self.main_menu_light_small = pygame.image.load("info_glow.png")
+        self.main_menu = pygame.image.load("main_menu.png")
+        self.button_glow = pygame.image.load("button_glow.png")
+        self.button_info_glow = pygame.image.load("button_info_glow.png")
 
-        self.options_menu = pygame.image.load("menu_options.png")
-        self.options_menu_back = pygame.image.load("options_back_liquid.png")
-        self.options_menu_liquid = pygame.image.load("options_liquid.png")
+        self.options_menu = pygame.image.load("options_menu.png")
+        self.options_back_liquid = pygame.image.load("options_back_liquid.png")
+        self.options_liquid = pygame.image.load("options_liquid.png")
 
-        self.info_menu = pygame.image.load("menu_info.png")
+        self.info_menu = pygame.image.load("info_menu.png")
 
         self.screen = 1
         self.selected = 0
@@ -30,8 +30,8 @@ class Menu:
         self.player_two_pic = pygame.image.load("bike_2_" + str(self.player_two_bike) + ".png")
         self.player_two_pic = pygame.transform.rotate(self.player_two_pic, 90)
 
-    def click(self, game, button, pos):
-        self.check_selected(pos)
+    def click(self, game, button, position):
+        self.check_selected(position)
         if self.screen == 1:
             if self.selected == 1:
                 game.init_players()
@@ -83,72 +83,74 @@ class Menu:
                 self.screen = 1
                 self.selected = 0
 
-    def check_selected(self, pos):
+    def check_selected(self, position):
         self.selected = 0
         if self.screen == 1:
-            if pos[0] in range(120, 341) and pos[1] in range(280, 341):
+            if position[0] in range(120, 341) and position[1] in range(280, 341):
                 self.selected = 1
-            elif pos[0] in range(160, 381) and pos[1] in range(380, 441):
+            elif position[0] in range(160, 381) and position[1] in range(380, 441):
                 self.selected = 2
-            elif pos[0] in range(200, 421) and pos[1] in range(480, 541):
+            elif position[0] in range(200, 421) and position[1] in range(480, 541):
                 self.selected = 3
-            elif pos[0] in range(670, 741) and pos[1] in range(480, 541):
+            elif position[0] in range(670, 741) and position[1] in range(480, 541):
                 self.selected = 4
                 
         elif self.screen == 2:
-            if pos[0] in range(74, 110) and pos[1] in range(204, 240):
+            if position[0] in range(74, 110) and position[1] in range(204, 240):
                 self.selected = 11
-            elif pos[0] in range(219, 255) and pos[1] in range(204, 240):
+            elif position[0] in range(219, 255) and position[1] in range(204, 240):
                 self.selected = 12
-            elif pos[0] in range(540, 576) and pos[1] in range(204, 240):
+            elif position[0] in range(540, 576) and position[1] in range(204, 240):
                 self.selected = 21
-            elif pos[0] in range(683, 719) and pos[1] in range(204, 240):
+            elif position[0] in range(683, 719) and position[1] in range(204, 240):
                 self.selected = 22
-            elif pos[0] in range(380, 416) and pos[1] in range(223, 259):
+            elif position[0] in range(380, 416) and position[1] in range(223, 259):
                 self.selected = 31
-            elif pos[0] in range(380, 416) and pos[1] in range(367, 403):
+            elif position[0] in range(380, 416) and position[1] in range(367, 403):
                 self.selected = 32
-            elif pos[0] in range(300, 525) and pos[1] in range(513, 572):
+            elif position[0] in range(300, 525) and position[1] in range(513, 572):
                 self.selected = 5
                 
         elif self.screen == 3:
-            if pos[0] in range(300, 525) and pos[1] in range(513, 572):
+            if position[0] in range(300, 525) and position[1] in range(513, 572):
                 self.selected = 1
                 
     def draw(self, screen):
         if self.screen == 1:
             screen.blit(self.main_menu, (0, 0))
             if self.selected in [1, 2, 3]:
-                screen.blit(self.main_menu_light, (103 + self.selected * 40, 180 + self.selected * 100))
+                screen.blit(self.button_glow, (103 + self.selected * 40, 180 + self.selected * 100))
             if self.selected == 4:
-                screen.blit(self.main_menu_light_small, (677, 480))
+                screen.blit(self.button_info_glow, (677, 480))
+                
         elif self.screen == 2:
-            screen.blit(self.options_menu_back, (334, 193))
-            screen.blit(self.options_menu_liquid, (333, 416 - self.speed * 25))
+            screen.blit(self.options_back_liquid, (334, 193))
+            screen.blit(self.options_liquid, (333, 416 - self.speed * 25))
             screen.blit(self.options_menu, (0, 0))
             screen.blit(self.player_one_pic, (157, 210))
             screen.blit(self.player_two_pic, (622, 210))
 
             if self.selected == 5:
-                screen.blit(self.main_menu_light, (323, 513))
+                screen.blit(self.button_glow, (323, 513))
+                
         elif self.screen == 3:
             screen.blit(self.info_menu, (0, 0))
             if self.selected == 1:
-                screen.blit(self.main_menu_light, (323, 513))
+                screen.blit(self.button_glow, (323, 513))
             
 
 class Point:
-    def __init__(self, pos, player_color, player_type):
-        self.pos = pos
+    def __init__(self, position, player_color, player_type):
+        self.position = position
         self.pic = pygame.image.load("bike_" + str(player_type + 1) + "_" + str(player_color) + "_l.png")
 
     def draw(self, screen):
-        screen.blit(self.pic, (int(self.pos[0] - 4), int(self.pos[1] - 4)))
+        screen.blit(self.pic, (int(self.position[0] - 4), int(self.position[1] - 4)))
 
 class Player:
-    def __init__(self, pos, player_color, player_type, speed):
-        self.pos = pos
-        self.path = []
+    def __init__(self, position, player_color, player_type, speed):
+        self.position = position
+        self.trace = []
         self.move = [0,0,0,0]
         self.player_color = player_color
         self.player_type = player_type
@@ -161,49 +163,49 @@ class Player:
         self.back_pic = pygame.image.load("back.png")
 
     def add_point(self):
-        temp_point = Point(vec2d(self.pos[0], self.pos[1]), self.player_color, self.player_type)
-        self.path.append(temp_point)
+        temp_point = Point(vec2d(self.position[0], self.position[1]), self.player_color, self.player_type)
+        self.trace.append(temp_point)
 
     def update(self, other_player, width, height):
-        for point in other_player.path:
-            if (self.pos - point.pos).length < 10:
+        for point in other_player.trace:
+            if (self.position - point.position).length < 10:
                 self.alive = False
 
-        if len(self.path) == 81:
-            self.path = self.path[1:]
+        if len(self.trace) == 81:
+            self.trace = self.trace[1:]
 
         self.add_point()
 
         if self.move[0]:
-            self.pos[0] += self.speed
+            self.position[0] += self.speed
             self.image = self.base_pic
         elif self.move[1]:
-            self.pos[0] -= self.speed
+            self.position[0] -= self.speed
             self.image = pygame.transform.rotate(self.base_pic, 180)
         elif self.move[2]:
-            self.pos[1] -= self.speed
+            self.position[1] -= self.speed
             self.image = pygame.transform.rotate(self.base_pic, 90)
         elif self.move[3]:
-            self.pos[1] += self.speed
+            self.position[1] += self.speed
             self.image = pygame.transform.rotate(self.base_pic, 270)
 
         coord = [width, height]
         for x in coord:
-            if self.pos[coord.index(x)] < 0:
-                self.pos[coord.index(x)] = x
-            if self.pos[coord.index(x)] > x:
-                self.pos[coord.index(x)] = 0
+            if self.position[coord.index(x)] < 0:
+                self.position[coord.index(x)] = x
+            if self.position[coord.index(x)] > x:
+                self.position[coord.index(x)] = 0
 
     def draw(self, screen):  
-        if self.path:
-            previous_point = self.path[-1]
-            for point in self.path:
-                if not previous_point.pos == point.pos:
+        if self.trace:
+            previous_point = self.trace[-1]
+            for point in self.trace:
+                if not previous_point.position == point.position:
                     point.draw(screen)
-                    screen.blit(self.back_pic, (((point.pos[0]//20) - 1) * 20,
-                                                (((point.pos[1]//20) - 1) * 20)))
+                    screen.blit(self.back_pic, (((point.position[0]//20) - 1) * 20,
+                                                (((point.position[1]//20) - 1) * 20)))
                 previous_point = point
-        screen.blit(self.image, (int(self.pos[0] - 9), int(self.pos[1] - 9)))
+        screen.blit(self.image, (int(self.position[0] - 9), int(self.position[1] - 9)))
 
 
 class Starter(PygameHelper):
@@ -249,18 +251,18 @@ class Starter(PygameHelper):
                 self.menu.screen = 1
 
         
-    def mouseUp(self, button, pos):
+    def mouseUp(self, button, position):
         if not self.menu.screen:
             if not self.player_one.alive or not self.player_two.alive:
-                if (pos - vec2d(548, 355)).length < 18 and button == 1:
+                if (position - vec2d(548, 355)).length < 18 and button == 1:
                     self.game_speed += 1
                     self.player_one = Player(vec2d(100, 200), self.player_colors[0], 0, self.game_speed)
                     self.player_two = Player(vec2d(100, 300), self.player_colors[1], 1, self.game_speed)
         else:
-            self.menu.click(self, button, pos)
+            self.menu.click(self, button, position)
 
-    def mouseMotion(self, buttons, pos, rel):
-        self.menu.check_selected(pos)
+    def mouseMotion(self, buttons, position, rel):
+        self.menu.check_selected(position)
 
         
     def draw(self):
